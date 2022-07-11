37bb8e107bc893630284b267b5a8183410d58f11
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-07-11 13:49:47 -0700

    build ping-pong
    Co-Authored-By: alex-mcknight11 <97002051+alex-mcknight11@users.noreply.github.com>
    Co-Authored-By: Tyler Emmerson <tyleremmerson0216@gmail.com>

--- Program.cs
+++ Program.cs
@@ -4,6 +4,28 @@ public class Program
 {
   public static void Main()
   {
-    
+    Console.WriteLine("Enter a number:");
+    string inputNumber = Console.ReadLine();
+    int parsedInput = int.Parse(inputNumber);
+    for (int index = 1; index <= parsedInput; index ++){
+      if (index % 5 == 0 && index % 3 == 0) 
+      {
+        Console.WriteLine("ping-pong");
+      }
+      
+      else if (index % 3 == 0)
+      {
+        Console.WriteLine("ping");
+      }
+      
+      else if (index % 5 == 0)
+      {
+        Console.WriteLine("pong");
+      }
+      else 
+      {
+        Console.WriteLine(index);
+      }
+    }
   }
 }
